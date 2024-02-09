e03d0d97f29016c8e7bca8737ea2073d9b05c3ff
Ilya Andreev <140309026+NeoScripter@users.noreply.github.com>
2024-02-09 15:09:44 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// script.js
-console.log("Welcome to my simple website.");
+'use strict'
+
+document.getElementById('yesBtn').addEventListener('click', function() {
+    const responseElement = document.getElementById('response');
+    responseElement.innerHTML = '<img src="https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_700/MjAxNDg3OTQxODQxNzI1MDQ5/reasons-not-to-have-a-pet-hamster.webp" alt="A cute hamster" />';
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    const responseElement = document.getElementById('response');
+    responseElement.textContent = `Too bad, it's very cute!`;
+});
